d13818121e519c29ddb0b5b0b1a2c30acb1bbb85
nik-trifonov <102224540+nik-trifonov@users.noreply.github.com>
2022-12-06 17:54:27 +0200

    Update Task7.java

--- Nikolay/Task7.java
+++ Nikolay/Task7.java
@@ -2,20 +2,13 @@ public class Task7
 {
     public static void main(String[] args) {
         int rows, cols, sumRow, sumCol;
-
-        //Initialize matrix a
-
         int a[][] = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
-
-        //Calculates number of rows and columns present in given matrix
         rows = a.length;
         cols = a[0].length;
-
-        //Calculates sum of each row of given matrix
         for(int i = 0; i < rows; i++){
             sumRow = 0;
             for(int j = 0; j < cols; j++){
@@ -24,7 +17,6 @@ public class Task7
             System.out.println("Sum of " + (i+1) +" row: " + sumRow);
         }
 
-        //Calculates sum of each column of given matrix
         for(int i = 0; i < cols; i++){
             sumCol = 0;
             for(int j = 0; j < rows; j++){
