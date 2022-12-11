cb2eae47c4dbd6201e061fa48aa4fc17feb4ecd8
balantaim <martin.ned.atanasov@gmail.com>
2022-12-11 22:32:05 +0200

    format the code and refector same variables

--- Martin/Task.java
+++ Martin/Task.java
@@ -1,27 +1,27 @@
 package selenium;
 
-
 import java.util.Scanner;
 
 public class Task {
 	
-		public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		//Task 1
+		// Task 1
 		var radius = 5;
 		var square = 10;
 		var passage = 2;
 		task1(radius, square, passage);
-		
+
 		sc.close();
 	}
-	public static void task1(int R, int S, int K) {
+
+	public static void task1(int radius, int square, int passage) {
 		boolean checkQuestion = false;
-		if(R+K<=S/2) {
+		if (radius + passage <= square / 2) {
 			checkQuestion = true;
 		}
-		System.out.println(checkQuestion? "Yes, it is possible.":"No, it is not impossible.");
+		System.out.println(checkQuestion ? "Yes, it is possible." : "No, it is not impossible.");
 	}
 
 }
--- Martin/Task2.java
+++ Martin/Task2.java
@@ -6,24 +6,26 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		//Task 2
-		int number=0;
+
+		// Task 2
+		int number = 0;
 		boolean positive = true;
-		String sumStr ="";
-		if(sc.hasNextInt()) {
+		String sumStr = "";
+		if (sc.hasNextInt()) {
 			number = sc.nextInt();
-			if(number<0){ positive = false;}
-			sumStr = positive? "positive ":"negative ";
-			if(!positive) {
-				number  = Math.abs(number);
+			if (number < 0) {
+				positive = false;
 			}
-			if(number>9 && number<100) {
-				sumStr+="two-digit";
-			}else if (number >=0 && number <10) {
-				sumStr+="one_digit";
-			}else {
-				sumStr+="many-digits";
+			sumStr = positive ? "positive " : "negative ";
+			if (!positive) {
+				number = Math.abs(number);
+			}
+			if (number > 9 && number < 100) {
+				sumStr += "two-digit";
+			} else if (number >= 0 && number < 10) {
+				sumStr += "one_digit";
+			} else {
+				sumStr += "many-digits";
 			}
 			System.out.println(sumStr);
 		}
